817e90a5242026b636eeea8f8782e73db3824681
Vihaan <vihaan.shah@uconn.edu>
2023-03-30 17:06:50 -0400

    parsing linear G0/G1 commands

--- path.py
+++ path.py
@@ -1,8 +1,8 @@
 from gcodeparser import GcodeParser
 import math
 
-class path_planner():
-    def __init__(self, file_path, accuracy, acc_limit, dec_limit, speed_limit):
+class Planner():
+    def __init__(self, file_path, accuracy = 0, acc_limit = 0, dec_limit = 0, speed_limit = 0):
         '''Input: 
         file: gcode file path
         accuracy: minimum step size of the arm
@@ -11,12 +11,13 @@ class path_planner():
         speed_limit: maximum speed of the arm 
         '''
         self.x, self.y, self.z, self.f = 0, 0, 0, 0
-        self.gcode = read_gcode(file_path)
+        self.file_path = file_path
+        self.gcode = self.read_gcode(file_path)
         
     
     def read_gcode(self, file):
-        parser = GcodeParser(file_path)
-        with open('setup.gcode', 'r') as f:
+        parser = GcodeParser(file)
+        with open(file, 'r') as f:
             gcode = f.read()
 
         lines = GcodeParser(gcode).lines
@@ -26,25 +27,39 @@ class path_planner():
     def generate(self):
         ''' Go line by line of the gcode file and generate the path
         '''
-        for line in gcode:
-            if line.command_str == 'G28':
-                self.G28()
-            elif line.command_str == 'G0':
-                self.G0()
-            elif line.command_str == 'G1':
-                self.G1()
-            else:
+        for line in self.gcode:
+            try:
+                command = getattr(self, line.command_str)
+                return command(line)
+            except AttributeError:
                 print('Command not recognized: {}'.format(line.command_str))
+
     
+    def line_get_params(self, line):
+        if line.get_param('X'):
+            x = line.get_param('X')
+        if line.get_param('Y'):
+            y = line.get_param('Y')
+        if line.get_param('Z'):
+            z = line.get_param('Z')
+        if line.get_param('F'):
+            f = line.get_param('F')
+        
+        return x, y, z, f
+        
+    def not_found(self, line):
+        return 'Command not recognized: {}'.format(line.command_str)
     
-    def G28(self):
+    def G28(self, line):
         pass
     
-    def G0(self):
-        pass
+    def G0(self, line):
+        new_x, new_y, new_z, new_f = self.line_get_params(line)
+        return new_x, new_y
     
-    def G1(self):
-        pass
+    def G1(self, line):
+        new_x, new_y, new_z, new_f = self.line_get_params(line)
+        return new_x, new_y
 
 if "__main__" == __name__:
     parser()
